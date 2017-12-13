a830c5ad6c9d438c5ff96c4d9f6957b80f11eeac
Meilan Jess <Meilan@meilans-air.hsd1.co.comcast.net>
2017-12-12 21:12:16 -0700

    Initial Commit

---
+++ Uppercase/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Uppercase
+//
+//  Created by Meilan Jess on 12/12/17.
+//  Copyright © 2017 Meilan Jess. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
